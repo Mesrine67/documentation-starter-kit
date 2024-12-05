565b60ed52c886728205e2d4dd5b3f58f09298f1
PaPi <89279156+Mesrine67@users.noreply.github.com>
2024-12-05 21:12:29 +0100

    test

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright © 2024 PaPi Docs',
   },
 }
 
